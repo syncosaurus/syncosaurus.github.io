cd3b1af89960a92481cf0c3290624a5c643bf6bf
emargetis <erik.margetis@gmail.com>
2024-04-20 20:51:29 -0500

    feat (case study):
    cleaned up case study and added code snippets
    
    Co-authored-by: Alex Sklar <sklarfox@gmail.com>
    Co-authored-by: Franco Waisfeld <francowaisfeld@gmail.com>
    Co-authored-by: Joseph Liang <j3liang@icloud.com>

---
+++ src/pages/todo-list-mutators.js
@@ -0,0 +1,14 @@
+// export default {
+//   addTodo,
+//   removeTodo,
+// }
+
+// function addTodo(tx, { id, text }) {
+//   const todo = { id, text, complete: false }
+//   tx.set(`todo/${id}`, todo)
+//   return todo
+// }
+
+// function removeTodo(tx, { id }) {
+//   tx.delete(`todo/${id}`)
+// }

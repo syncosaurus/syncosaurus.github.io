d09b64502e7c102379a4e1042d5dbc50ec534be8
emargetis <erik.margetis@gmail.com>
2024-04-24 13:22:30 -0500

    bug (case study): fixed formatting on mobile
    Co-authored-by: Alex Sklar <sklarfox@gmail.com>
    Co-authored-by: Franco Waisfeld <francowaisfeld@gmail.com>
    Co-authored-by: Joseph Liang <j3liang@icloud.com>

--- src/theme/MDXPage/index.js
+++ src/theme/MDXPage/index.js
@@ -47,7 +47,7 @@ export default function MDXPage(props) {
                 />
               </div>
             )}
-            <div className={clsx('col', !hideTableOfContents && 'col--8')}>
+            <div className={clsx('col', !hideTableOfContents && 'col')}>
               {unlisted && <Unlisted />}
               <article>
                 <MDXContent>

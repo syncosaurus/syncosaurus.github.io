3ff389de1d133fe4b68b4076d80b9e8fc2d60b9d
Alex Sklar <sklarfox@gmail.com>
2024-04-24 17:04:05 -0700

    refactor(slider): adjust colors to match branding, resize to fit on mobile
    Co-authored-by: Erik Margetis <erik.margetis@gmail.com>
    Co-authored-by: Joseph Liang <j3liang@icloud.com>

--- src/components/LatencySlider/VerticalSliderContainer.js
+++ src/components/LatencySlider/VerticalSliderContainer.js
@@ -1,13 +1,13 @@
-import { styled, Box } from '@mui/system';
+import { styled, Box } from '@mui/system'
 
 const VerticalSliderContainer = styled(Box)(({ theme }) => ({
   display: 'inline-block',
   justifyContent: 'center',
   alignItems: 'center',
   padding: '3rem',
-  marginBottom: '0.75rem',
+  marginBottom: '0.25rem',
   marginTop: '0.75rem',
   gap: theme.spacing(50, 'auto'), // Adjust the gap between sliders
-}));
+}))
 
-export default VerticalSliderContainer;
+export default VerticalSliderContainer

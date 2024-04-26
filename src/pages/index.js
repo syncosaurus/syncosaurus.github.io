421bf9babe2ae295fc4a9c964025241b1f0beeab
jgoteam <j3liang@icloud.com>
2024-04-26 11:01:22 -0700

    style: moved font locations, update default font to Ubuntu, change 'case button' hover
    Co-authored-by: Alex Sklar <sklarfox@gmail.com>
    Co-authored-by: Erik Margetis <erik.margetis@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
   const handleScrollClick = () => {
     if (featureRef.current) {
-      featureRef.current.scrollIntoView({ behavior: 'smooth' });
+      featureRef.current.scrollIntoView({ behavior: 'smooth' })
     }
   }
 
@@ -20,34 +20,36 @@ export default function Home() {
   return (
     <Layout description="Syncosaurus Homepage">
       <div
-      id="hero-landing"
-      className="font-sans h-11/12 rounded-3xl lg:rounded-tl-2xl lg:rounded-bl-2xl shadow-md overflow-hidden m-1 pt-10 pb-5 flex flex-col text-center items-center bg-gradient-to-br from-slate-900 via-slate-800 to-syncoBlue"
-    >
-      <Puzzle />
-      <h1 className="font-jakarta text-6xl font-bold text-white m-8">{siteConfig.title}</h1>
-      <p className="text-2xl text-white min-w-96 max-w-screen-lg">
-        {siteConfig.tagline}
-      </p>
-      <div className="pt-2 pb-20">
-        <Link
-          href="#_"
-          className="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-xl text-white font-lg font-800 bg-syncoGreen hover:shadow-lg syncoGreen-400/500 rounded-md hover:syncoGreen-800 hover:no-underline sm:w-auto sm:mb-0"
-          data-primary="green-500"
-          data-rounded="rounded-3xl"
-          data-primary-reset="{}"
-          to="/case-study"
-        >
-          Read Case Study
-        </Link>
+        id="hero-landing"
+        className="font-sans h-11/12 rounded-3xl lg:rounded-tl-2xl lg:rounded-bl-2xl shadow-md overflow-hidden m-1 pt-10 pb-5 flex flex-col text-center items-center bg-gradient-to-br from-slate-900 via-slate-800 to-syncoBlue"
+      >
+        <Puzzle />
+        <h1 className="text-6xl font-bold text-white m-8">
+          {siteConfig.title}
+        </h1>
+        <p className="text-2xl text-white min-w-96 max-w-screen-lg">
+          {siteConfig.tagline}
+        </p>
+        <div className="pt-2 pb-20 items-center">
+          <Link
+            href="#_"
+            className="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-xl text-white font-lg font-800 bg-syncoGreen hover:bg-green-400 rounded-md hover:text-white hover:no-underline sm:w-auto sm:mb-0"
+            data-primary="green-500"
+            data-rounded="rounded-3xl"
+            data-primary-reset="{}"
+            to="/case-study"
+          >
+            Read Case Study
+          </Link>
+        </div>
+        <div className="pt-8 pb-0">
+          <img
+            className="cursor-pointer opacity-10 animate-pulse"
+            src="/img/icons/scroll-down-arrow.png"
+            onClick={handleScrollClick}
+          ></img>
+        </div>
       </div>
-      <div className="pt-8 pb-0">
-        <img
-          className="cursor-pointer opacity-10 animate-pulse"
-          src="/img/icons/scroll-down-arrow.png"
-          onClick={handleScrollClick}
-        ></img>
-      </div>
-    </div>
       <main ref={featureRef}>
         <HomepageBenefits />
         <HomepageFeatures />
